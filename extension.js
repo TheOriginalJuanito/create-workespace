c38992dbdb1469a4aa8bcada9253f160c048c70b
TheOriginalJuanito <jon.m.bisaz@gmail.com>
2025-07-28 19:38:43 +0200

    Startet

--- extension.js
+++ extension.js
@@ -1,36 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const fs = require('fs');
+const path = require('path');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log('Extension "create-workspace" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "create-workespace" is now active!');
+  const createWebdevBase = vscode.commands.registerCommand('create-workspace.createWebdevBase', async (uri) => {
+    const baseFolder = await getTargetFolder(uri);
+    if (!baseFolder) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('create-workespace.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    const folderName = await vscode.window.showInputBox({ prompt: 'Name of the project folder:' });
+    if (!folderName) return;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Create-Workespace!');
-	});
+    const targetPath = path.join(baseFolder.fsPath, folderName);
+    fs.mkdirSync(targetPath, { recursive: true });
 
-	context.subscriptions.push(disposable);
+    fs.writeFileSync(path.join(targetPath, 'index.html'), '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <link rel="stylesheet" href="style.css">\n    <title>Document</title>\n</head>\n<body>\n\n\n</body>\n</html>');
+    fs.writeFileSync(path.join(targetPath, 'style.css'), 'body {\n  font-family: sans-serif;\n}');
+    fs.writeFileSync(path.join(targetPath, 'script.js'), 'console.log("Hello Webdev!");');
+
+    vscode.window.showInformationMessage(`Created Webdev Base in ${folderName}`);
+  });
+
+  const HTML = vscode.commands.registerCommand('create-workspace.HTML', async (uri) => {
+    const baseFolder = await getTargetFolder(uri);
+    if (!baseFolder) return;
+
+    const fileName = await vscode.window.showInputBox({ prompt: 'Name of the HTML file (without .HTML):' });
+    if (!fileName) return;
+
+    const mdxPath = path.join(baseFolder.fsPath, `${fileName}.html`);
+    const content = '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <link rel="stylesheet" href="style.css">\n    <title>Document</title>\n</head>\n<body>\n\n\n</body>\n</html>';
+
+    fs.writeFileSync(mdxPath, content);
+
+    vscode.window.showInformationMessage(`Created ${fileName}.mdx`);
+  });
+
+  context.subscriptions.push(createWebdevBase, HTML);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
-module.exports = {
-	activate,
-	deactivate
+async function getTargetFolder(uri) {
+  if (uri && uri.fsPath) {
+    return uri;
+  }
+  const folders = vscode.workspace.workspaceFolders;
+  if (!folders || folders.length === 0) {
+    vscode.window.showErrorMessage('Please open a workspace folder or use the context menu.');
+    return null;
+  }
+  return folders[0].uri;
 }
+
+module.exports = {
+  activate,
+  deactivate
+};
